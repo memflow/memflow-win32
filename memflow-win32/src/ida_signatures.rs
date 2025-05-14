e44509f231f05f9d99bc5c85b94b9e07fccea969
ConnorBP <connor.postma@gmail.com>
2025-05-14 13:08:01 -0400

    created handy ida signature macro

---
+++ memflow-win32/src/ida_signatures.rs
@@ -0,0 +1,34 @@
+use regex::Regex;
+
+/// Generates a regex string from an ida-like hex string.
+/// Made by [@ConnorBP] :)
+/// Main benefit of this is that you can directly use your ida signatures,
+/// while not needing to decode them at runtime or manually beforehand.
+/// 
+/// Example Usage:
+/// ```
+/// const REGEX_PATTERN: &str = ida_regex![4C 8B ? ? ? ? 3C 9F];
+/// let re = Regex::new(REGEX_PATTERN).unwrap();
+/// assert_eq!(REGEX_PATTERN, "(?-u)\\x4C\\x8B(?s:.){4}\\x3C\\x9F");
+/// ```
+#[macro_export]
+macro_rules! ida_regex {
+    ( $($token:tt)+ ) => {
+        concat!(
+            "(?-u)", // Search with unicode code-points disabled. \\x4C is actually equal to the hex byte 0x4C
+            $(
+                $crate::ida_regex_part!($token)
+            ),*
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! ida_regex_part {
+    (?) => {
+        "(?s:.)"
+    };
+    ($hex:tt) => {
+        concat!("\\x", stringify!($hex))
+    };
+}
